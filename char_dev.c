a334144751b5b446c1d085865aa9e4120519c756
Reshar97 <reshar97@gmail.com>
2026-04-25 18:14:37 +0300

    Рефакторинг: упрощена структура устройства, буфер выделен статически. Использован kzalloc для обнуления памяти при аллокации.

--- char_dev.c
+++ char_dev.c
@@ -4,29 +4,38 @@
 #include <linux/cdev.h>
 #include <linux/device.h>
 #include <linux/uaccess.h>
+#include <linux/init.h>
+#include <linux/ioctl.h>
+#include <linux/slab.h>
 
 #define DEVICE_NAME "my_buffer"
 #define CLASS_NAME  "my_class"
 #define BUF_SIZE    1024
 
+/* Уникальные коды команд для ioctl */
+#define IOCTL_GETBUFFER_SIZE _IOR('b', 1, int32_t*)
+#define IOCTL_GET_USED_SPACE _IOR('b', 2, int32_t*)
+#define IOCTL_GET_FREE_SPACE _IOR('b', 3, int32_t*)
+#define IOCTL_CLEAR_BUFFER _IO('b', 4, int32_t*)
+
 /* СТРУКТУРА ДАННЫХ */
-struct my_device_data {
+struct ring_buffer_dev {
     unsigned char buffer[BUF_SIZE];
-    int head;
-    int tail;
+    int read_ptr;
+    int write_ptr;
+    int bytes_in_buffer;
     struct mutex lock;
     struct cdev cdev;
-};
+} *my_device_data; 
 
-/* ПРОТОТИПЫ ФУНКЦИЙ (TODO)
-   Объявляем все функции */
+/* ПРОТОТИПЫ ФУНКЦИЙ */
 static int      dev_open(struct inode *, struct file *);
 static int      dev_release(struct inode *, struct file *);
 static ssize_t  dev_read(struct file *, char *, size_t, loff_t *);
 static ssize_t  dev_write(struct file *, const char *, size_t, loff_t *);
 static long     dev_ioctl(struct file *, unsigned int, unsigned long);
 
-/* ФАЙЛОВЫЕ ОПЕРАЦИИ (ГОТОВО)
+/* ФАЙЛОВЫЕ ОПЕРАЦИИ
    Связка имен функций с системными вызовами. */
 static struct file_operations fops = {
     .owner   = THIS_MODULE,
@@ -37,24 +46,55 @@ static struct file_operations fops = {
     .release = dev_release,
 };
 
-/* ГЛОБАЛЬНЫЕ ПЕРЕМЕННЫЕ (ГОТОВО)
+/* ГЛОБАЛЬНЫЕ ПЕРЕМЕННЫЕ
    Переменные для регистрации устройства. */
 static int major_number;
 static struct class* my_class = NULL;
 static struct device* my_device = NULL;
 
-/* ИНИЦИАЛИЗАЦИЯ МОДУЛЯ (TODO) 
-  Эта функция вызывается при команде 'insmod'.
-  TODO: Выделить память, зарегистрировать устройство. */
+/* ИНИЦИАЛИЗАЦИЯ МОДУЛЯ */
 static int __init char_dev_demo_init(void) {
-    printk(KERN_INFO "Driver: Инициализация (TODO)\n");
+    printk(KERN_INFO "RingBuffer: Инициализация \n");
 
-    // TODO: Реализовать регистрацию главного номера устройства
-    // major_number = ...
+    /* Выделяем память только под структуру */
+    my_device_data = kzalloc(sizeof(struct ring_buffer_dev), GFP_KERNEL);
+    if (!my_device_data) {
+        return -ENOMEM;
+    }
 
-    // TODO: Реализовать создание класса и устройства
-    // my_class = ...
-    // my_device = ...
+    /* Инициализируем переменные */
+    my_device_data->read_ptr = 0;
+    my_device_data->write_ptr = 0;
+    my_device_data->bytes_in_buffer = 0;
+    mutex_init(&my_device_data->lock);
+
+    /*  Регистрируем устройство */
+    major_number = register_chrdev(0, DEVICE_NAME, &fops);
+    if (major_number < 0) {
+        kfree(my_device_data);
+        printk(KERN_ALERT "RingBuffer: Ошибка регистрации\n");
+        return major_number;
+    }
+
+    /* Создаем класс и устройство в /dev/ */
+    my_class = class_create(THIS_MODULE, CLASS_NAME);
+    if (IS_ERR(my_class)) {
+        unregister_chrdev(major_number, DEVICE_NAME);
+        kfree(my_device_data);
+        return PTR_ERR(my_class);
+    }
+
+    my_device = device_create(my_class, NULL, MKDEV(major_number, 0), NULL, DEVICE_NAME);
+    if (IS_ERR(my_device)) {
+        class_destroy(my_class);
+        unregister_chrdev(major_number, DEVICE_NAME);
+        kfree(my_device_data);
+        return PTR_ERR(my_device);
+    }
+
+    printk(KERN_INFO "RingBuffer: Устройство /dev/%s создано\n", DEVICE_NAME);
+    return 0;
+}
 
     if (/* TODO: Проверка, что все создалось успешно */) {
         printk(KERN_INFO "Driver: Устройство /dev/%s создано\n", DEVICE_NAME);
